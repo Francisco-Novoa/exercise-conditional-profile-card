a23a4c75800b74f4d1af19b61f4bba20c5a3bcb4
Francisco Novoa Parada <francisco.novoa.14@sansano.usm.cl>
2020-03-02 14:15:06 -0300

    proyecto terminado

--- src/js/index.js
+++ src/js/index.js
@@ -9,19 +9,40 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  if (variables.name == null) variables.name = "Gendo";
+  if (variables.lastname == null) variables.lastname = "Ikari";
+  if (variables.role == null) variables.role = "Nerv Director";
+  if (variables.city == null) variables.city = "Tokio 3";
+  if (variables.country == null) variables.country = "Japan";
+  if (variables.socialMediaPosition == "position-left")
+    variables.socialMediaPosition = "position-left";
+  if (variables.twitter == null) variables.twitter = "tele1313";
+  if (variables.github == null)
+    variables.github = "terriblehackskeyboard/keyboard";
+  if (variables.linkedin == null)
+    variables.linkedin = "in/gendo-ikari-b1b63486";
+  if (variables.instagram == null) variables.instagram = "evangelion__memes";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}; ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a target="_blank" href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fa fa-twitter"></i></a></li>
+            <li><a target="_blank" href="https://github.com/${
+              variables.github
+            }"><i class="fa fa-github"></i></a></li>
+            <li><a target="_blank" href="https://linkedin.com/${
+              variables.linkedin
+            }"><i class="fa fa-linkedin"></i></a></li>
+            <li><a target="_blank" href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -35,14 +56,16 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://vignette.wikia.nocookie.net/evangelion/images/a/a3/Nerv-logo.jpg/revision/latest/scale-to-width-down/340?cb=20110215172843&path-prefix=es",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://pm1.narvii.com/7072/bcaf2c55d9f671da41fd9884bea8c136fe201496r1-259-194v2_uhq.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "left",
+    socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
